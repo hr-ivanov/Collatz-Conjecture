f47113387043705cb287c014592d8247aecf68f2
hr-ivanov <102746056+hr-ivanov@users.noreply.github.com>
2022-07-09 20:16:51 +0300

    Add files via upload

---
+++ Collatz Conjecture/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CollatzConjecture
+{
+	class Program
+	{
+		static int order = 0;
+		public static void Generate(int n)
+        {
+			Console.WriteLine("№ {0}. - {1}",order,n);
+			order++;
+			if (n % 2 == 0)
+			{
+				Generate(n/2);
+			}
+			else if (n == 1)
+			{
+				return;
+			}
+			else
+            {
+				Generate((3 * n) + 1);
+			}
+        }
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter a whole number: ");
+			int number = int.Parse(Console.ReadLine());
+			Generate(number);
+
+			Console.WriteLine("Hello world!");
+		}
+	}
+}
