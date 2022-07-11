d62cf8c8ab93107771ffcd6420ec1928178ec6ef
hr-ivanov <102746056+hr-ivanov@users.noreply.github.com>
2022-07-11 13:49:24 +0300

    Biginteger settings

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+namespace CollatzConjecture
+{
+	class Program
+	{
+		static int order = 0;
+		public static void Generate(BigInteger n)
+        {
+			Console.WriteLine("№ {0}. - {1}",order,n);
+			order++;
+			if (n % 2 == 0)
+			{
+				Generate(n/2);
+			}
+			else if (n == 1)
+			{
+				return;
+			}
+			else
+            {
+				Generate((3 * n) + 1);
+			}
+        }
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter a whole number: ");
+			BigInteger number = BigInteger.Parse(Console.ReadLine());
+			Generate(number);
+
+			Console.WriteLine("Hello world!");
+		}
+	}
+}
